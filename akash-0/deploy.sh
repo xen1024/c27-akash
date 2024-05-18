9b77a0d633f56858aee612fbe2b31a7c359b330f
web3cryptowallet <web3cryptowallet@nohash.org>
2024-05-18 21:58:37 +0300

    ready to debug

--- akash-0/deploy.sh
+++ akash-0/deploy.sh
@@ -1,11 +1,22 @@
+. ./config-akash
+
 echo Configuring deploy.yaml
 echo Your SSH_PUBKEY is $SSH_PUBKEY
 
-
-. ./config-akash
-
 echo TODO: verify SSH_PUBKEY
 
-cat deploy-template-0.yaml | sed s/SSH_KEY_PLACEHOLDER/$SSH_PUBKEY/g > deploy.yaml
+echo DOCKER_CONTAINER=$DOCKER_CONTAINER
+echo NETUID=$NETUID
+echo NETWORK=$NETWORK
+echo WALLET_NAME=$WALLET_NAME
+echo WALLET_HOTKEY=$WALLET_HOTKEY
 
+cat deploy-template-0.yaml | sed -e s/SSH_KEY_PLACEHOLDER/$SSH_PUBKEY/g \
+-e s/DOCKER_CONTAINER_PLACEHOLDER/$DOCKER_CONTAINER/g \
+-e s/MINER_PLACEHOLDER/$MINER/g \
+-e s/VALIDATOR_PLACEHOLDER/$VALIDATOR/g \
+-e s/NETUID_PLACEHOLDER/$NETUID/g \
+-e s/NETWORK_PLACEHOLDER/$NETWORK/g \
+-e s/WALLET_NAME_PLACEHOLDER/$WALLET_NAME/g \
+-e s/WALLET_HOTKEY_PLACEHOLDER/$WALLET_HOTKEY/g > deploy.yaml
 
--- akash-0/startup.sh
+++ akash-0/startup.sh
@@ -1,11 +1,33 @@
 echo "$SSH_PUBKEY" > /root/.ssh/authorized_keys
 
+run_miner () {
+  cd neurons
+  python -m miner.py 
+    --netuid $NETUID  # The subnet id you want to connect to
+    --subtensor.network $NETWORK  # blockchain endpoint you want to connect
+    --wallet.name $WALLET_NAME # name of your wallet
+    --wallet.hotkey $WALLET_HOTKEY # hotkey name of your wallet
+    --logging.debug # Run in debug mode, alternatively --logging.trace for trace mode
+}
+
+run_validator () {
+  cd neurons
+  python -m validator.py 
+    --netuid $NETUID # The subnet id you want to connect to
+    --subtensor.network $NETWORK # blockchain endpoint you want to connect
+    --wallet.name $WALLET_NAME  # name of your wallet
+    --wallet.hotkey $WALLET_HOTKEY # hotkey name of your wallet
+    --logging.debug # Run in debug mode, alternatively --logging.trace for trace mode
+}
+
 if [ $MINER ]; then
   echo Starting miner
+  run_miner
 fi
 
 if [ $VALIDATOR ]; then
   echo Starting validator
+  run_validator
 fi
 
 
