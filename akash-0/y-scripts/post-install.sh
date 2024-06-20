bf6651c82826b02e028481d09309b2f4ef5d1132
web3cryptowallet <web3cryptowallet@nohash.org>
2024-06-20 09:30:47 +0300

    post-install up

--- akash-0/y-scripts/post-install.sh
+++ akash-0/y-scripts/post-install.sh
@@ -2,4 +2,9 @@
 . ~/.config-akash
 
 echo Configuring ssh key
+mkdir -p /root/.ssh
+chmod 700 /root/.ssh
 echo "ssh-rsa $SSH_PUBKEY root" > /root/.ssh/authorized_keys
+RUN chmod 700 /root/.ssh/authorized_keys
+RUN mkdir -p /var/run/sshd
+RUN chmod 0755 /var/run/sshd
