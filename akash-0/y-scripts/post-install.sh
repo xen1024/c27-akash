4693429c2cfba8d403f6acc24756490120bed64b
web3cryptowallet <web3cryptowallet@nohash.org>
2024-06-20 09:37:57 +0300

    post up

--- akash-0/y-scripts/post-install.sh
+++ akash-0/y-scripts/post-install.sh
@@ -5,6 +5,6 @@ echo Configuring ssh key
 mkdir -p /root/.ssh
 chmod 700 /root/.ssh
 echo "ssh-rsa $SSH_PUBKEY root" > /root/.ssh/authorized_keys
-RUN chmod 700 /root/.ssh/authorized_keys
-RUN mkdir -p /var/run/sshd
-RUN chmod 0755 /var/run/sshd
+chmod 700 /root/.ssh/authorized_keys
+mkdir -p /var/run/sshd
+chmod 0755 /var/run/sshd
